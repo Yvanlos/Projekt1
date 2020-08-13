bf43130501e2d92cd2535243d1ed5ab5428042ee
Julian Ameling <sopr069.sopra@cs.uni-dortmund.de>
2020-08-13 19:12:38 +0200

    TeamsView hinzugefügt

---
+++ src/view/TeamsViewController.java
@@ -0,0 +1,20 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class TeamsViewController {
+
+    @FXML
+    private ListView<?> teamListView;
+
+    @FXML
+    private Button addTeamButton;
+
+    @FXML
+    void addTeamButtonEvent(ActionEvent event) {
+
+    }
+}
