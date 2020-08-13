817742b1def065d5347798a0b526da9ee780691d
Julian Ameling <sopr069.sopra@cs.uni-dortmund.de>
2020-08-13 12:55:31 +0200

    MainView hinzugefügt

---
+++ src/view/MainViewController.java
@@ -0,0 +1,43 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+
+public class MainViewController {
+
+    @FXML
+    private Button createProjectButton;
+
+    @FXML
+    private Button teamsButton;
+
+    @FXML
+    private Button developersButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    void onCreateProjectButtonEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDevelopersButtonEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onExitButtonEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTeamsButtonEvent(ActionEvent event) {
+
+    }
+
+}
