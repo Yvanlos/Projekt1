1b1055d112456ceec627c1533caa45492daaf1e1
Julian Ameling <sopr069.sopra@cs.uni-dortmund.de>
2020-08-13 14:11:34 +0200

    KanBanView hinzugefügt

---
+++ src/view/KanBanViewController.java
@@ -0,0 +1,32 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+
+public class KanBanViewController {
+
+    @FXML
+    private Button infoButton;
+
+    @FXML
+    private Button addTaskButton;
+
+    @FXML
+    private Button archiveButton;
+
+    @FXML
+    void onAddTaskButtonEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onArchiveButtonEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onInfoButtonEvent(ActionEvent event) {
+
+    }
+}
