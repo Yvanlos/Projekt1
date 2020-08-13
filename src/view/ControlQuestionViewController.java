a5dbad5006108f193a978920d03d020f5de35763
Janina Rau <sopr079.sopra@cs.uni-dortmund.de>
2020-08-13 16:24:45 +0200

    Added ControlQuestionView and ControlQuestionViewController

---
+++ src/view/ControlQuestionViewController.java
@@ -0,0 +1,25 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControlQuestionViewController {
+
+    @FXML
+    private Button continueButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void onCancelButtonEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onContinueButtonEvent(ActionEvent event) {
+
+    }
+
+}
