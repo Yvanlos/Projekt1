96f870b00593c646784b5beefd497b892a0ee4f4
sopr061 <sopr061.sopra@cs.uni-dortmund.de>
2020-08-14 02:24:52 +0200

    Kommentar schreiben und Kommentar lesen

---
+++ src/view/NewCommentController.java
@@ -0,0 +1,24 @@
+package view;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class NewCommentController {
+
+    @FXML
+    private Button okayButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void OnCancelButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onOkayButtonClicked(MouseEvent event) {
+
+    }
+
+}
