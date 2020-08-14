af8eb7465c4cc50d12f8d0b3d18e3d0650383393
Robert Huber <sopr060.sopra@cs.uni-dortmund.de>
2020-08-14 09:18:53 +0200

    adds DeveloperListView and DeveloperListViewController

---
+++ src/view/DeveloperListViewController.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DeveloperListViewController {
+
+    @FXML
+    private Button addDeveloperButton;
+
+    @FXML
+    void onAddDeveloperButtonAction(ActionEvent event) {
+
+    }
+
+}
