6035c0aa2b6a53511e735c66810c202c07dcccc2
Martin Yvan Owono Owono <sopr066.sopra@cs.uni-dortmund.de>
2020-08-17 00:41:44 +0200

    Change it totaly and add only two buttom : Export and Cancel

---
+++ src/view/pdfViewController.java
@@ -0,0 +1,26 @@
+package view;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class pdfViewController {
+
+   @FXML
+   private Button export;
+
+   @FXML
+   private Button cancel;
+
+   @FXML
+   void theExportEvent (ActionEvent event){
+
+   }
+
+   @FXML
+   void theCancelEvent (ActionEvent event){
+
+   }
+
+}
