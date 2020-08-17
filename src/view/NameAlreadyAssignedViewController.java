de841f6f3d1e5a4df51a120e41433bb31b5d5bc1
Robin Edmaier <sopr062.sopra@cs.uni-dortmund.de>
2020-08-17 16:02:21 +0200

    Added Error-Messages for name already assigned

---
+++ src/view/NameAlreadyAssignedViewController.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class NameAlreadyAssignedViewController {
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    void onOKButtonMouseClick(MouseEvent event) {
+
+    }
+
+}
+
