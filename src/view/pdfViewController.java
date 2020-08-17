c0f56d2cd3b85a27c7c8aa6e5f3f47c66e56d2cf
Martin Yvan Owono <martinyvan.owono@yahoo.fr>
2020-08-17 04:19:38 +0200

    zwei CheckBox für die beiden Fälle wurden hinzugefügt, alle Tafeln oder ein Tafel auswählen und eine SplitMenuButton tafelauswahl wurde ebenfalls hinzugefügt.

--- src/view/pdfViewController.java
+++ src/view/pdfViewController.java
@@ -4,6 +4,8 @@ package view;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.skin.SplitMenuButtonSkin;
 
 public class pdfViewController {
 
@@ -12,6 +14,30 @@ public class pdfViewController {
 
    @FXML
    private Button cancel;
+
+   @FXML
+   private CheckBox allTafeln;
+
+   @FXML
+   private CheckBox einTafel;
+
+   @FXML
+   private SplitMenuButtonSkin tafelAuswahl;
+
+   @FXML
+   void theAllTafelnEvent (ActionEvent event){
+
+   }
+
+   @FXML
+   void theEinTafelEvent (ActionEvent event){
+
+   }
+
+   @FXML
+   void theTafelAuswahl(ActionEvent event){
+
+   }
 
    @FXML
    void theExportEvent (ActionEvent event){
