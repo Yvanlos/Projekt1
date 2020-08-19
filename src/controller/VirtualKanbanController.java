3d9c5f9cd60739d964ce133c635da3457e3fd9db
Robin Edmaier <sopr062.sopra@cs.uni-dortmund.de>
2020-08-20 00:01:27 +0200

    Implemented constructor and methods in VirtualKanbanController

--- src/controller/VirtualKanbanController.java
+++ src/controller/VirtualKanbanController.java
@@ -6,44 +6,116 @@ import java.lang.UnsupportedOperationException;
 public class VirtualKanbanController {
 
     /**
- 	 * Reference to the ProjectController, witch can be used by other controllers, the GUI or the VirtualKanban-Class
+ 	 * Reference to the ProjectController, which can be used by other controllers, the GUI or the VirtualKanban-Class
  	 */
     private ProjectController projectController;
 
     /**
- 	 * Reference to the TaskController, witch can be used by other controllers, the GUI or the VirtualKanban-Class
+ 	 * Reference to the TaskController, which can be used by other controllers, the GUI or the VirtualKanban-Class
  	 */
     private TaskController taskController;
 
     /**
- 	 * Reference to the DeveloperController, witch can be used by other controllers, the GUI or the VirtualKanban-Class
+ 	 * Reference to the DeveloperController, which can be used by other controllers, the GUI or the VirtualKanban-Class
  	 */
     private DeveloperController developerController;
 
     /**
- 	 * Reference to the TeamController, witch can be used by other controllers, the GUI or the VirtualKanban-Class
+ 	 * Reference to the TeamController, which can be used by other controllers, the GUI or the VirtualKanban-Class
  	 */
     private TeamController teamController;
 
     /**
- 	 * Reference to the IOController, witch can be used by other controllers, the GUI or the VirtualKanban-Class
+ 	 * Reference to the IOController, which can be used by other controllers, the GUI or the VirtualKanban-Class
  	 */
     private IOController iOController;
 
     /**
- 	 * Reference to the VirtualKanban (Main-Class form Model)
+ 	 * Reference to the VirtualKanban (Main-Class of model)
  	 */
     private VirtualKanban virtualKanban;
 
     /**
- 	 * Reference to the StatisticController, witch can be used by other controllers, the GUI or the VirtualKanban-Class
+ 	 * Reference to the StatisticController, which can be used by other controllers, the GUI or the VirtualKanban-Class
  	 */
     private StatisticController statisticController;
 
+    /**
+     * Constructor of the virtualKanbanController
+     *
+     * TODO: replace 'new VirtualKanban()' with the loading mechanism from IOController
+     */
     public VirtualKanbanController() {
+        projectController = new ProjectController(this);
+        taskController = new TaskController(this);
+        developerController = new DeveloperController(this);
+        teamController = new TeamController(this);
+        iOController = new IOController(this);
+        statisticController = new StatisticController(this);
+
+        virtualKanban = new VirtualKanban(); // has to be replaced with loading feature
     }
 
+    /**
+     * Returns the ProjectController, which can be used by other controllers, the GUI or the VirtualKanban-Class
+     *
+     * @return ProjectController
+     */
+    public ProjectController getProjectController() {
+        return projectController;
+    }
+
+    /**
+     * Returns the TaskController, which can be used by other controllers, the GUI or the VirtualKanban-Class
+     *
+     * @return TaskController
+     */
+    public TaskController getTaskController() {
+        return taskController;
+    }
+
+    /**
+     * Returns the DeveloperController, which can be used by other controllers, the GUI or the VirtualKanban-Class
+     *
+     * @return DeveloperController
+     */
+    public DeveloperController getDeveloperController() {
+        return developerController;
+    }
+
+    /**
+     * Returns the TeamController, which can be used by other controllers, the GUI or the VirtualKanban-Class
+     *
+     * @return TeamController
+     */
+    public TeamController getTeamController() {
+        return teamController;
+    }
+
+    /**
+     * Returns the IOController, which can be used by other controllers, the GUI or the VirtualKanban-Class
+     *
+     * @return IOController
+     */
+    public IOController getIOController() {
+        return iOController;
+    }
+
+    /**
+     * Returns the VirtualKanban, which is the Main-Class of the model
+     *
+     * @return VirtualKanban
+     */
     public VirtualKanban getVirtualKanban() {
         return virtualKanban;
+    }
+
+    /**
+     * Returns the StatisticController, which can be used by other controllers, the GUI or the VirtualKanban-Class
+     *
+     * @return StatisticController
+     */
+    public StatisticController getStatisticController() {
+        return statisticController;
     }
 }
