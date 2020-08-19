5a5710575a7a4d34cc832d6b3d649db72dd86d44
Lars-Ole Eppert <sopr065.sopra@cs.uni-dortmund.de>
2020-08-19 12:54:19 +0200

    implements methods in Team.java

--- src/model/Team.java
+++ src/model/Team.java
@@ -2,7 +2,11 @@ package model;
 
 import java.util.Collection;
 import java.lang.UnsupportedOperationException;
+import java.util.List;
 
+/**
+ * A team is a group of developers
+ */
 public class Team {
 
     /**
@@ -13,31 +17,45 @@ public class Team {
     /**
  	 * list of developers in the team
  	 */
-    private Collection<Developer> developer;
+    private List<Developer> developers;
 
     /**
  	 *	adds a developer to the team
 	 *
- 	 * TODO: create JavaDoc. 
+ 	 *
  	 * @param developer
 	 * 					developer, which is added to the team
- 	 * @throws UnsupportedOperationException
- 	 *	 	 	Diese Exception wird geworfen, fallsdie Methode noch nicht implementiert ist. 
+ 	 *
  	 */
-    public void addDeveloper(Developer developer) throws UnsupportedOperationException {
-        throw new UnsupportedOperationException("Not Yet Implemented!");
+    public void addDeveloper(Developer developer) {
+        this.developers.add(developer);
     }
 
     /**
  	 * removes a developer from the team
 	 *
- 	 * TODO: create JavaDoc. 
+ 	 *
  	 * @param developer
 	 * 					developer, which is removed from the team
- 	 * @throws UnsupportedOperationException
- 	 *	 	 	Diese Exception wird geworfen, fallsdie Methode noch nicht implementiert ist. 
+ 	 *
  	 */
-    public void removeDeveloper(Developer developer) throws UnsupportedOperationException {
-        throw new UnsupportedOperationException("Not Yet Implemented!");
+    public void removeDeveloper(Developer developer) {
+        this.developers.remove(developer);
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public List<Developer> getDevelopers() {
+		return developers;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDevelopers(List<Developer> developers) {
+		this.developers = developers;
+	}
 }
