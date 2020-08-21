5887723d360debf5f8bef11f6272dbcb4dd9960f
Lars-Ole Eppert <sopr065.sopra@cs.uni-dortmund.de>
2020-08-21 12:54:57 +0200

    Changes in RankingView.fxml + RankingViewController

--- src/view/RankingViewController.java
+++ src/view/RankingViewController.java
@@ -3,10 +3,15 @@ package view;
 import controller.VirtualKanbanController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import model.Project;
+
 
 import java.lang.UnsupportedOperationException;
 
@@ -34,6 +39,23 @@ public class RankingViewController extends BorderPane {
     void initialize() {
         this.minWidthProperty().bind(stackPane.widthProperty());
         this.minHeightProperty().bind(stackPane.heightProperty());
+    }
+
+    @FXML
+    private TableView<?> tableViewaRanking;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnName;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnLast7Days;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void onBackButtonEvent(MouseEvent event) {
+
     }
 
 }
