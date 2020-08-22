825e08f42d8f116ff6fe7557c98c65d7077ac983
Janina Rau <sopr079.sopra@cs.uni-dortmund.de>
2020-08-22 14:10:14 +0200

    Implements a subMenu for all unassigned developers in startTask, disables all buttons if project is archived, refactors NewTaskViewController

--- src/view/KanBanViewController.java
+++ src/view/KanBanViewController.java
@@ -123,22 +123,46 @@ public class KanBanViewController extends BorderPane {
             }
         }
 
-        //Adds all tasks in a stageList in the specific Box, starting from new
-        addTasksToStageBox(newStageBox, stageList);
-        stageList = project.getNextStage(stageList);
-        addTasksToStageBox(analysisInProgressBox, stageList);
-        stageList = project.getNextStage(stageList);
-        addTasksToStageBox(analysisFinishedBox, stageList);
-        stageList = project.getNextStage(stageList);
-        addTasksToStageBox(implementationInProgressBox, stageList);
-        stageList = project.getNextStage(stageList);
-        addTasksToStageBox(implementationFinishedBox, stageList);
-        stageList = project.getNextStage(stageList);
-        addTasksToStageBox(testInProgressBox, stageList);
-        stageList = project.getNextStage(stageList);
-        addTasksToStageBox(testFinishedBox, stageList);
-        stageList = project.getNextStage(stageList);
-        addTasksToStageBox(finishedStageBox, stageList);
+        //If project is not archived enable all buttons
+        if(!project.isReadOnly()){
+            //Adds all tasks in a stageList in the specific Box, starting from new
+            addTasksToStageBox(newStageBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBox(analysisInProgressBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBox(analysisFinishedBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBox(implementationInProgressBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBox(implementationFinishedBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBox(testInProgressBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBox(testFinishedBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBox(finishedStageBox, stageList);
+        }else{//If project is archived, disable all buttons that edit a project
+            addTaskButton.setDisable(true);
+            archiveButton.setDisable(true);
+            //Adds all tasks in a stageList in the specific Box, starting from new, enables only read comments
+            addTasksToStageBoxProjectArchived(newStageBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBoxProjectArchived(analysisInProgressBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBoxProjectArchived(analysisFinishedBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBoxProjectArchived(implementationInProgressBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBoxProjectArchived(implementationFinishedBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBoxProjectArchived(testInProgressBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBoxProjectArchived(testFinishedBox, stageList);
+            stageList = project.getNextStage(stageList);
+            addTasksToStageBoxProjectArchived(finishedStageBox, stageList);
+
+
+        }
 
         //Adds all unassigned developers to the unassigned List
         //TODO: use something else than a label
@@ -152,7 +176,7 @@ public class KanBanViewController extends BorderPane {
 
 
     /**
-     * Adds all tasks in the given stageList to the given VBox
+     * Adds all tasks in the given stageList to the given VBox, if project is not archived
      *
      * @param box       the VBox the tasks should be added to
      * @param stageList the stageList that holds the to be added tasks
@@ -166,7 +190,11 @@ public class KanBanViewController extends BorderPane {
             }else{
                 menuButton.setText(task.getName() + "\n" + task.getDescription() + "\nDeadline: " + task.getDeadline().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + "\nEntwickler: nicht zugewiesen");
             }
-            menuButton.getItems().add(new MenuItem("Aufgabe anfangen"));
+            //A menu that shows all unassigned developers is added, if a developer is chosen he is assigned to the task
+            Menu menu = new Menu("Aufgabe anfangen");
+            menuButton.getItems().add(menu);
+            addUnassignedDevelopersAsSubMenu(menu,task);
+
             menuButton.getItems().add(new MenuItem("Aufgabe abbrechen"));
             menuButton.getItems().add(new MenuItem("Aufgabe beenden"));
             menuButton.getItems().add(new MenuItem("Aufgabe l\u00f6schen"));
@@ -186,21 +214,6 @@ public class KanBanViewController extends BorderPane {
             }
             //If the task is not currently worked on, the buttons for finishing and droping are disabled but start task can be chosen
             else {
-                menuButton.getItems().get(0).setOnAction(event -> {
-                    //Get a ComboBox with all developers
-                    ArrayList<Developer> devList = project.getTeam().getDevelopers();
-                    //TODO Filter all assigned developer
-                    ObservableList<Developer> observableDevList = FXCollections.observableArrayList(devList);
-
-                    ComboBox<Developer> comboBox = new ComboBox<Developer>(observableDevList);
-                    comboBox.setPromptText("Bitte Entwickler auswählen");
-                    box.getChildren().add(comboBox);
-                    //TODO Choose developer
-                    //comboBox.on
-
-                    virtualKanbanController.getTaskController().startTask(task, project, new Developer("TestDev", null));
-                    box.getChildren().remove(comboBox);
-                });
                 menuButton.getItems().get(1).setDisable(true);
                 menuButton.getItems().get(2).setDisable(true);
                 //If the task is completed, all buttons for actions are disabled
@@ -208,12 +221,10 @@ public class KanBanViewController extends BorderPane {
                     menuButton.getItems().get(0).setDisable(true);
                 }
             }
-            //If deleteComment is chosen...
+            //If deleteComment is chosen a confirm window is shown
             menuButton.getItems().get(3).setOnAction(event -> {
                 controlQuestionViewController = new ControlQuestionViewController(virtualKanbanController, "deleteTaskButton", project, task);
                 controlQuestionViewController.showView();
-
-                //TODO ControlQuestion
             });
             //If readComment is chosen all other comment windows are closed and the readCommentView is shown
             menuButton.getItems().get(4).setOnAction(event -> {
@@ -239,6 +250,66 @@ public class KanBanViewController extends BorderPane {
             });
             //the menuButton is added to the VBox
             box.getChildren().add(menuButton);
+        });
+    }
+
+    /**
+     * Adds all buttons to the given stagelist, but disables them, because the project is archived
+     *
+     * @param box       the VBox the tasks should be added to
+     * @param stageList the stageList that holds the to be added tasks
+     */
+    public void addTasksToStageBoxProjectArchived(VBox box, StageList stageList) {
+        stageList.getTask().forEach(task -> {
+            //A MenuButton with 6 MenuItems for managing tasks is added for each task in this stage
+            MenuButton menuButton = new MenuButton();
+            menuButton.setText(task.getName() + "\n" + task.getDescription() + "\nDeadline: " + task.getDeadline().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + "\nEntwickler: nicht zugewiesen");
+            menuButton.getItems().add(new MenuItem("Aufgabe anfangen"));
+            menuButton.getItems().add(new MenuItem("Aufgabe abbrechen"));
+            menuButton.getItems().add(new MenuItem("Aufgabe beenden"));
+            menuButton.getItems().add(new MenuItem("Aufgabe l\u00f6schen"));
+            menuButton.getItems().add(new MenuItem("Kommentare anzeigen"));
+            menuButton.getItems().add(new MenuItem("Kommentar hinzuf\u00fcgen"));
+
+            //Disable all buttons that are not allowed to be used in an archived project
+            menuButton.getItems().get(0).setDisable(true);
+            menuButton.getItems().get(1).setDisable(true);
+            menuButton.getItems().get(2).setDisable(true);
+            menuButton.getItems().get(3).setDisable(true);
+            menuButton.getItems().get(5).setDisable(true);
+
+            //If readComment is chosen all other comment windows are closed and the readCommentView is shown
+            menuButton.getItems().get(4).setOnAction(event -> {
+                if (readCommentController != null) {
+                    readCommentController.closeView();
+                }
+                readCommentController = new ReadCommentController(virtualKanbanController, task);
+                readCommentController.showView();
+            });
+
+            //the menuButton is added to the VBox
+            box.getChildren().add(menuButton);
+        });
+    }
+
+    /**
+     * Adds for every unassigned developer a menuItem that starts a task with the chosen developer
+     * @param menu the menu, the menuItems should be added to
+     * @param task the task, the menu belongs to
+     */
+    public void addUnassignedDevelopersAsSubMenu(Menu menu, Task task) {
+        project.getTeam().getDevelopers().forEach(developer -> {
+            if (!developer.isAtWork()) {
+                MenuItem menuItem = new MenuItem(developer.getName());
+                menuItem.setOnAction(event -> {
+                    virtualKanbanController.getTaskController().startTask(task, project, developer);
+                });
+                menu.getItems().add(menuItem);
+            }
+            if(menu.getItems().isEmpty()){
+                MenuItem menuNoDevAvailable = new MenuItem("Alle Entwickler aktuell zugewiesen");
+                menu.getItems().add(menuNoDevAvailable);
+            }
         });
     }
 
