362ab163de2c9a3acf2d42ed630e3e20c29b0647
Julian Ameling <sopr069.sopra@cs.uni-dortmund.de>
2020-08-23 15:28:32 +0200

    corrected the addTask method and added some tests

--- tests/controller/TaskControllerTest.java
+++ tests/controller/TaskControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 import static org.junit.Assert.*;
 
@@ -17,7 +19,7 @@ public class TaskControllerTest {
 
     /**
      *
-     * @throws Exception
+     * @throws Exception if the creation was unsuccessful
      */
     @Before
     public void setUp() throws Exception {
@@ -58,14 +60,20 @@ public class TaskControllerTest {
         String d= "description";
         LocalDateTime dateTime= LocalDateTime.now();
         tc.addTask(p,n,d,dateTime);
-        Task t = new Task(n,d,dateTime);
-        for(StageList list : p.getStageList())
-        {
-            if(list.getTask().contains(t))
-            {
-                assertTrue(list.getTask().contains(t));
-            }
-        }
+        assertEquals(p.getStageList().get(0).getTask().size(),1);
+    }
+
+    /**
+     * tests addTask with a tasks name which already exists in that project
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddTaskUnsussessful() {
+        Project p = new Project("testController","descriptionController", LocalDateTime.now(), new Team("testname"));
+        String n= "name";
+        String d= "description";
+        LocalDateTime dateTime= LocalDateTime.now();
+        tc.addTask(p,n,d,dateTime);
+        tc.addTask(p,n,d,dateTime);
     }
 
     /**
@@ -80,6 +88,17 @@ public class TaskControllerTest {
     }
 
     /**
+     * tests addNote with a note which already exists
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddNoteUnsussessful() {
+        Task t= new Task("testController","descriptionController",LocalDateTime.now());
+        Note n= new Note("testnama","testcontent",LocalDateTime.now());
+        tc.addNote(t,n);
+        tc.addNote(t,n);
+    }
+
+    /**
      * tests the method startTask(task, project, developer)
      */
     @Test
@@ -90,6 +109,20 @@ public class TaskControllerTest {
         p.getStageList().get(0).getTask().add(t);
         tc.startTask(t,p,d);
         assertTrue(t.isInProgress());
+    }
+
+    /**
+     * tests the method startTask(task, project, developer) wiht a task which is already in progress
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void startTaskUnsuccessful() {
+        Project p = new Project("testController","descriptionController", LocalDateTime.now(), new Team("testname"));
+        Task t= new Task("testController","descriptionController",LocalDateTime.now());
+        Developer d= new Developer("testname", URI.create("image"));
+        Developer d2= new Developer("testname2", URI.create("image"));
+        p.getStageList().get(0).getTask().add(t);
+        tc.startTask(t,p,d);
+        tc.startTask(t,p,d2);
     }
 
     /**
@@ -115,6 +148,17 @@ public class TaskControllerTest {
     }
 
     /**
+     * tests the method finishTask(task, project) with a task which is not in work
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void finishTaskUnsuccessful() {
+        Project p = new Project("testController","descriptionController", LocalDateTime.now(), new Team("testname"));
+        Task t= new Task("testController","descriptionController",LocalDateTime.now());
+        p.getStageList().get(0).getTask().add(t);
+        tc.finishTask(t,p);
+    }
+
+    /**
      * tests the method dropTask(task, project)
      */
     @Test
@@ -135,10 +179,28 @@ public class TaskControllerTest {
     }
 
     /**
+     * tests the method dropTask(task, project) with a task which is not in work
+     */
+    @Test (expected = NoSuchElementException.class)
+    public void dropTaskUnsuccessful() {
+        Project p = new Project("testController","descriptionController", LocalDateTime.now(), new Team("testname"));
+        Task t= new Task("testController","descriptionController",LocalDateTime.now());
+        p.getStageList().get(0).getTask().add(t);
+        tc.dropTask(t,p);
+    }
+
+    /**
      * tests the method showNotes()
      */
     @Test
     public void showNotes() {
+        Task t= new Task("testController","descriptionController",LocalDateTime.now());
+        Note n= new Note("testnama","testcontent",LocalDateTime.now());
+        tc.addNote(t,n);
+        ArrayList<Note> correctList = new ArrayList<>();
+        correctList.add(n);
+
+        assertEquals(tc.showNotes(t),correctList);
     }
 
     /**
@@ -146,6 +208,22 @@ public class TaskControllerTest {
      */
     @Test
     public void testGetStageFromTask(){
+        Project p = new Project("testController","descriptionController", LocalDateTime.now(), new Team("testname"));
+        Task t= new Task("testController","descriptionController",LocalDateTime.now());
+        p.getStageList().get(1).getTask().add(t);
+
+        assertEquals(tc.getStageFromTask(t,p),p.getStageList().get(1).getStage());
+    }
+
+    /**
+     * tests the method getStageFromTask(task, project)
+     * with a task which is not part of any stageList
+     */
+    @Test (expected = NoSuchElementException.class)
+    public void testGetStageFromTaskUncucsessful(){
+        Project p = new Project("testController","descriptionController", LocalDateTime.now(), new Team("testname"));
+        Task t= new Task("testController","descriptionController",LocalDateTime.now());
+        tc.getStageFromTask(t,p);
 
     }
 }
