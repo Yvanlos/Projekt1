9e758896110ec16ba1e9237687a87130c58500c6
Martin Yvan Owono <martinyvan.owono@yahoo.fr>
2020-08-24 00:57:53 +0200

    Die Methode exportATable() is implementiert worden.

--- src/controller/IOController.java
+++ src/controller/IOController.java
@@ -1,60 +1,48 @@
 package controller;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
 import model.*;
 
 import java.io.*;
-import java.lang.UnsupportedOperationException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.util.ArrayList;
+//import sandbox.WrapToTest;
 
-/**
- * controller to save and load the programms status and export PDF's
- */
 public class IOController {
 
 	/**
 	 * the file in which the status is saved
 	 */
 	private static final File SAVE_File = new File("save");
+	private static final File src = new File("KanbanBoard");
 
 	/**
 	 * the file in which the PDF exports are saved
 	 */
-	private static final File Table_PDF = new File("?");
+	//private static final File DEST = new File("KanbanBoard");
 
-    /**
- 	 * Reference to the VirtualKanbanController, that every controller has
- 	 */
-    private VirtualKanbanController virtualKanbanController;
+	/**
+	 * Reference to the VirtualKanbanController, that every controller has
+	 */
+	private VirtualKanbanController virtualKanbanController;
 
-    public IOController(VirtualKanbanController virtualKanbanController) {
-    	this.virtualKanbanController =  virtualKanbanController ;
-    }
+	public IOController(VirtualKanbanController virtualKanbanController) {
+		this.virtualKanbanController = virtualKanbanController;
+	}
 
-    /**
- 	 *
- 	 * loads the last state of the Program after launching VirtualKanban
- 	 * @throws IOException
- 	 *	 	 	is thrown if an error occurs while loading
- 	 */
-    public VirtualKanban load() throws IOException{
+	/**
+	 * loads the last state of the Program after launching VirtualKanban
+	 *
+	 * @throws IOException is thrown if an error occurs while loading
+	 */
+	public VirtualKanban load() throws IOException {
 		if (SAVE_File.exists()) {
 			try {
 				ObjectInputStream stream = new ObjectInputStream(new FileInputStream(SAVE_File));
 				VirtualKanban virtualKanban = (VirtualKanban) stream.readObject();
 				stream.close();
 				return virtualKanban;
-			} catch(IOException exception) {
+			} catch (IOException exception) {
 				throw new IOException("error loading file");
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
@@ -63,135 +51,64 @@ public class IOController {
 		return null;
 	}
 
-    /**
- 	 *
- 	 * saves the current state of the program when it gets closed
- 	 * @throws IOException
- 	 *	 	 	is thrown if an error occurs while saving a file
- 	 */
-    public void save() throws IOException {
-		try{
+	/**
+	 * saves the current state of the program when it gets closed
+	 *
+	 * @throws IOException is thrown if an error occurs while saving a file
+	 */
+	public void save() throws IOException {
+		try {
 			ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(SAVE_File));
 			stream.writeObject(virtualKanbanController.getVirtualKanban());
 			stream.close();
-		} catch (IOException ioex ) {
+		} catch (IOException ioex) {
 			System.err.println("Fehler beim Speichern aufgetreten.");
 			ioex.printStackTrace();
 		}
-    }
-
-	/***
-	 *
-	 * @param allproject is the list of all the project of the program
-	 * @throws FileNotFoundException will be call if a File can't be found
-	 * @throws DocumentException will be call if there's a problem with this document
-	 */
-	public void exportPdf(ArrayList<Project> allproject) throws FileNotFoundException, DocumentException {
-    	Document document = new Document() ;
-    	FileOutputStream place =  new FileOutputStream(Table_PDF) ;/* is this the correct DESTINATION ? */
-    	PdfWriter.getInstance(document,place);
-
-		for(Project x : allproject) {
-			document.open();
-			Paragraph intro = new Paragraph(x.getName());
-			Paragraph space = new Paragraph("  ");
-			PdfPTable table = new PdfPTable(8);
-			// creating cells for the table
-			PdfPCell c1 = new PdfPCell(new Paragraph(Stage.NEW.name()));
-			PdfPCell c2 = new PdfPCell(new Paragraph(Stage.ANALYSE_IN_PROGRESS.name()));
-			PdfPCell c3 = new PdfPCell(new Paragraph(Stage.ANALYSE_FINISHED.name()));
-			PdfPCell c4 = new PdfPCell(new Paragraph(Stage.IMPLEMENTATION_IN_PROGRESS.name()));
-			PdfPCell c5 = new PdfPCell(new Paragraph(Stage.IMPLEMENTATION_FINISHED.name()));
-			PdfPCell c6 = new PdfPCell(new Paragraph(Stage.TEST_IN_PROGRESS.name()));
-			PdfPCell c7 = new PdfPCell(new Paragraph(Stage.TEST_FINISHED.name()));
-			PdfPCell c8 = new PdfPCell(new Paragraph(Stage.COMPLETED.name()));
-			//add cells to the table
-			PdfPCell [] CELS ={c1,c2,c3,c4,c5,c6,c7,c8};
-			int i= CELS.length;
-			while (i>0){table.addCell(CELS[i--]);}
-			StageList stgl1 = new StageList(Stage.NEW);
-			StageList stgl2 = new StageList(Stage.ANALYSE_IN_PROGRESS);
-			StageList stgl3 = new StageList(Stage.ANALYSE_FINISHED);
-			StageList stgl4 = new StageList(Stage.IMPLEMENTATION_IN_PROGRESS);
-			StageList stgl5 = new StageList(Stage.IMPLEMENTATION_FINISHED);
-			StageList stgl6 = new StageList(Stage.TEST_IN_PROGRESS);
-			StageList stgl7 = new StageList(Stage.TEST_FINISHED);
-			StageList stgl8 = new StageList(Stage.COMPLETED);
-			int j=0;
-			int index = 0 ;
-			StageList [] STGL = {stgl1,stgl2,stgl3,stgl4,stgl5,stgl6,stgl7,stgl8};
-			// add in the table all task that are in a Stage
-			for(StageList s : STGL ) {
-				ArrayList<Task> TaskForAStageList = s.getTask();
-				for (Task t : TaskForAStageList) {
-					CELS[j] = new PdfPCell(new Paragraph(t.toString()));
-					//add the task t to the table
-					table.addCell(CELS[j]);
-				}
-				j++;
-			}
-			// now adding the Object to the Document :
-			document.add(intro);
-			document.add(space);
-			document.add(table);
-			//closing the document with
-			document.close();
+	}
+	public void exportAllTable(File dest,Project [] projects) throws IOException, DocumentException {
+		for(Project project : projects ) {
+			exportATable(dest, project);
 		}
 	}
-
-	/***
-	 *
-	 * @param project , i'm doing a table of this project
-	 * @throws FileNotFoundException will be call if a File can't be found
-	 * @throws DocumentException will be call if there's a problem with this document
-	 */
-	public void exportPdf(Project project) throws FileNotFoundException, DocumentException {
-        Document document = new Document() ;
-		FileOutputStream place =  new FileOutputStream(Table_PDF) ;/* is this the correct destination ? */
-		PdfWriter.getInstance(document,place);
+	public void exportATable(File dest,Project project) throws IOException, DocumentException {
+		Document document = new Document(PageSize.A4,0,0,0,0 );
+		PdfWriter.getInstance(document, new FileOutputStream(dest));
 		document.open();
-		Paragraph intro = new Paragraph(project.getName());
-		Paragraph space = new Paragraph("  ");
+
+		document.setPageSize(PageSize.A4);
+		document.newPage();
+
 		PdfPTable table = new PdfPTable(8);
-		// creating cells for the table
-		PdfPCell c1 = new PdfPCell(new Paragraph(Stage.NEW.name()));
-		PdfPCell c2 = new PdfPCell(new Paragraph(Stage.ANALYSE_IN_PROGRESS.name()));
-		PdfPCell c3 = new PdfPCell(new Paragraph(Stage.ANALYSE_FINISHED.name()));
-		PdfPCell c4 = new PdfPCell(new Paragraph(Stage.IMPLEMENTATION_IN_PROGRESS.name()));
-		PdfPCell c5 = new PdfPCell(new Paragraph(Stage.IMPLEMENTATION_FINISHED.name()));
-		PdfPCell c6 = new PdfPCell(new Paragraph(Stage.TEST_IN_PROGRESS.name()));
-		PdfPCell c7 = new PdfPCell(new Paragraph(Stage.TEST_FINISHED.name()));
-		PdfPCell c8 = new PdfPCell(new Paragraph(Stage.COMPLETED.name()));
-		//add cells to the table
-		PdfPCell [] CELS ={c1,c2,c3,c4,c5,c6,c7,c8};
-		int i= CELS.length;
-		while (i>0){table.addCell(CELS[i--]);}
-		StageList stgl1 = new StageList(Stage.NEW);
-		StageList stgl2 = new StageList(Stage.ANALYSE_IN_PROGRESS);
-		StageList stgl3 = new StageList(Stage.ANALYSE_FINISHED);
-		StageList stgl4 = new StageList(Stage.IMPLEMENTATION_IN_PROGRESS);
-		StageList stgl5 = new StageList(Stage.IMPLEMENTATION_FINISHED);
-		StageList stgl6 = new StageList(Stage.TEST_IN_PROGRESS);
-		StageList stgl7 = new StageList(Stage.TEST_FINISHED);
-		StageList stgl8 = new StageList(Stage.COMPLETED);
-		int j=0;
-		int index = 0 ;
-		StageList [] STGL = {stgl1,stgl2,stgl3,stgl4,stgl5,stgl6,stgl7,stgl8};
-		// add in the table all task that are in a Stage
-		for(StageList s : STGL ) {
-			ArrayList<Task> TaskForAStageList = s.getTask();
-			for (Task t : TaskForAStageList) {
-				CELS[j] = new PdfPCell(new Paragraph(t.toString()));
-				//add the task t to the table
-				table.addCell(CELS[j]);
+		table.setWidthPercentage(100);
+		table.setSpacingBefore(0f);
+		table.setSpacingAfter(0f);
+		PdfPCell cell = new PdfPCell(new Phrase("Project"+" "+project.getName()));
+		cell.setColspan(8);
+		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+		cell.setPadding(06.0f);
+		cell.setBackgroundColor(new BaseColor(144, 221, 8));
+		table.addCell(cell);
+		//projects = virtualKanbanController.getVirtualKanban().getProject() ;
+		ArrayList<StageList> stages = project.getStageList() ;
+		for (int aw = 0; aw < stages.size(); aw++) {
+			table.addCell(stages.get(aw).getStage().name());
 			}
-			j++;
+		for(StageList stageslist : stages) {
+			ArrayList<Task> tasks = stageslist.getTask();
+			for(Task task : tasks){
+				table.addCell(task.getName());
+			}
+
 		}
-		// now adding the Object to the Document :
-		document.add(intro);
-		document.add(space);
+
 		document.add(table);
-		//closing the document with
 		document.close();
 	}
 }
+
+
+
+
+
+
--- tests/controller/IOControllerTest.java
+++ tests/controller/IOControllerTest.java
@@ -1,43 +1,122 @@
 package controller;
 
+import com.itextpdf.text.DocumentException;
+import model.Project;
+import model.StageList;
+import model.Task;
+import model.Team;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
 
-/**
- * tests the IOController
- */
-public class IOControllerTest {
+public class IOControllerTest{
 
-    /**
-     * tests the method exportPdf(project) which exports only one project
-     */
-    @Test
-    public void testExportPdf1() {
-
+   VirtualKanbanController vkc;
+    IOController io;
+    private static final File DEST = new File("KanbanBoard");
+    public IOControllerTest(){
+        vkc = new VirtualKanbanController();
+        io = new IOController(vkc);
     }
 
-    /**
-     * tests the method exportPdf(projects) which exports every project
-     */
     @Test
-    public void testExportPdf2() {
+    public void testExportATable() throws Exception {
+        LocalDateTime dateTimeP1 = LocalDateTime.of(2020, Month.AUGUST, 23, 16, 15, 15) ;
+        LocalDateTime dateTimeP2 = LocalDateTime.of(2020, Month.AUGUST, 24, 16, 15, 15) ;
+        LocalDateTime dateTimeP3 = LocalDateTime.of(2020, Month.AUGUST, 25, 16, 15, 15) ;
+        LocalDateTime dateTimeP4 = LocalDateTime.of(2020, Month.AUGUST, 26, 16, 15, 15) ;
+        LocalDateTime dateTimeP5 = LocalDateTime.of(2020, Month.AUGUST, 23, 10, 15, 15) ;
+        LocalDateTime dateTimeP6 = LocalDateTime.of(2020, Month.AUGUST, 25, 10, 15, 15) ;
+        LocalDateTime dateTimeP7 = LocalDateTime.of(2020, Month.AUGUST, 26, 10, 15, 15) ;
 
-    }
 
-    /**
-     * tests the method save()
-     */
-    @Test
-    public void testSave() {
+        Team team1 = new Team("a");
+        Team team2 = new Team("b");
+        Team team3 = new Team("c");
+        Team team4 = new Team("d");
+        Project p1 = new Project("vacation","gone trip",dateTimeP1,team1) ;
+        Project p2 = new Project("game","fun",dateTimeP2,team2) ;
+        Project p3 = new Project("work out","healthy",dateTimeP3,team3) ;
+        Project p4 = new Project("end","back trip",dateTimeP4,team4) ;
+        ArrayList<Project> projects = new ArrayList<>();
+        projects.add(p1);
+        projects.add(p2);
+        projects.add(p3);
+        projects.add(p4);
 
-    }
+        // 4 Task pro projekte .
 
-    /**
-     * tests teh method load()
-     */
-    @Test
-    public void testLoad() {
+        Task t1 = new Task("job1","all of us",dateTimeP5);
+        Task t2 = new Task("job2","all of us",dateTimeP1);
+        Task t3 = new Task("job3","all of us",dateTimeP2);
+        Task t4 = new Task("job4","all of us",dateTimeP3);
+        Task t5 = new Task("job5","all of us",dateTimeP4);
+        Task t6 = new Task("job6","all of us",dateTimeP2);
+        Task t7 = new Task("job7","all of us",dateTimeP5);
+        Task t8 = new Task("job8","all of us",dateTimeP6);
+        Task t9 = new Task("job9","all of us",dateTimeP6);
+        Task t10 = new Task("job10","all of us",dateTimeP6);
+        Task t11= new Task("job11","all of us",dateTimeP6);
+        Task t12 = new Task("job12","all of us",dateTimeP6);
+        Task t13= new Task("job13","all of us",dateTimeP6);
+        Task t14= new Task("job14","all of us",dateTimeP6);
+        Task t15= new Task("job15","all of us",dateTimeP6);
+        Task t16= new Task("job16","all of us",dateTimeP6);
+        Task t17= new Task("job17","all of us",dateTimeP4);
+        Task t18= new Task("job18","all of us",dateTimeP6);
+        Task t19 = new Task("job19","all of us",dateTimeP6);
+        Task t20 = new Task("job20","all of us",dateTimeP6);
+        Task t21 = new Task("job21","all of us",dateTimeP3);
+        Task t22= new Task("job22","all of us",dateTimeP5);
+        Task t23= new Task("job23","all of us",dateTimeP6);
+        Task t24 = new Task("job24","all of us",dateTimeP5);
+        Task t25= new Task("job25","all of us",dateTimeP4);
+        Task t26= new Task("job26","all of us",dateTimeP3);
+        Task t27= new Task("job27","all of us",dateTimeP5);
+        Task t28= new Task("job28","all of us",dateTimeP1);
+        Task t29= new Task("job29","all of us",dateTimeP2);
+        Task t30= new Task("job30","all of us",dateTimeP3);
+        Task t31= new Task("job31","all of us",dateTimeP5);
+        Task t32 = new Task("job32","all of us",dateTimeP6);
+        Task t33= new Task("job33","all of us",dateTimeP7);
+        Task t34= new Task("job34","all of us",dateTimeP7);
+        Task t35= new Task("job35","all of us",dateTimeP6);
+        Task t36= new Task("job36","all of us",dateTimeP1);
+        Task t37= new Task("job37","all of us",dateTimeP2);
+        Task t38= new Task("job38","all of us",dateTimeP5);
+        Task t39= new Task("job39","all of us",dateTimeP3);
+        Task t40= new Task("job40","all of us",dateTimeP4);
 
+        Task [] tasks = {t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,
+                         t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26,
+                        t27,t28,t29,t30,t31,t32,t33,t34,t35,t36,t37,t38,t39,t40};
+        ArrayList<StageList> stageListsP1 = p1.getStageList();
+        ArrayList<StageList> stageListsP2 = p2.getStageList();
+        ArrayList<StageList> stageListsP3 = p3.getStageList();
+        ArrayList<StageList> stageListsP4 = p4.getStageList();
+        int stageIndex = 0;
+        for(int i = 0 ; i < tasks.length ; i++) {
+            stageListsP1.get(stageIndex).addTask(tasks[i]);
+        }
+        for(int i = 0 ; i < tasks.length ; i++) {
+            stageListsP2.get(stageIndex).addTask(tasks[i]);
+        }
+        for(int i = 0 ; i < tasks.length ; i++) {
+            stageListsP3.get(stageIndex).addTask(tasks[i]);
+        }
+        for(int i = 0 ; i < tasks.length ; i++) {
+            stageListsP4.get(stageIndex).addTask(tasks[i]);
+        }
+        Project [] projectArrayList = {p1,p2,p3,p4} ;
+        File file = new File(String.valueOf(DEST));
+        io.exportATable(DEST,p4);
+        //io.exportAllTable(DEST,projectArrayList);
     }
 }
