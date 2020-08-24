1eb673dd3da886d872c631a71a2befb08488e648
sopr061 <sopr061.sopra@cs.uni-dortmund.de>
2020-08-24 12:22:36 +0200

    RankingViewController implemented

--- src/view/RankingViewController.java
+++ src/view/RankingViewController.java
@@ -1,19 +1,21 @@
 package view;
 
 import controller.VirtualKanbanController;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
-import model.Project;
 
-
-import java.lang.UnsupportedOperationException;
+import java.util.HashMap;
 
 public class RankingViewController extends BorderPane {
 
@@ -21,13 +23,13 @@ public class RankingViewController extends BorderPane {
     private Button backButton;
 
     @FXML
-    private TableView<?> tableViewRanking;
+    private TableView<RankingViewData> tableViewRanking;
 
     @FXML
-    private TableColumn<?, ?> tableColumnName;
+    private TableColumn<RankingViewData, String> tableColumnName;
 
     @FXML
-    private TableColumn<?, ?> tableColumnLast7Days;
+    private TableColumn<RankingViewData, Integer> tableColumnLast7Days;
 
     private StackPane stackPane;
 
@@ -47,10 +49,40 @@ public class RankingViewController extends BorderPane {
         }
     }
 
+    public class RankingViewData {
+        public SimpleStringProperty name;
+        public SimpleIntegerProperty completedTasks;
+        public RankingViewData(String name, int completedTasks) {
+            this.name = new SimpleStringProperty(name);
+            this.completedTasks = new SimpleIntegerProperty(completedTasks);
+        }
+        public int getCompletedTasks() {return completedTasks.get();}
+        public String getName() {return name.get();}
+        public void setCompletedTasks(int completedTasks) {
+            this.completedTasks.set(completedTasks);
+        }
+        public void setName(String name) {
+            this.name.set(name);
+        }
+    }
+
     @FXML
     void initialize() {
         this.minWidthProperty().bind(stackPane.widthProperty());
         this.minHeightProperty().bind(stackPane.heightProperty());
+        HashMap<String, Integer> map = virtualKanbanController.getStatisticController().showRanking();
+        RankingViewData[] data = new RankingViewData[map.keySet().size()];
+        int index = 0;
+        for(String name : map.keySet()) {
+            data[index] = new RankingViewData(name, map.get(name));
+            index++;
+            //System.out.println(name + " " + map.get(name));
+        }
+        ObservableList<RankingViewData> list = FXCollections.observableArrayList(data);
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<RankingViewData, String>("name"));
+        tableColumnLast7Days.setCellValueFactory(new PropertyValueFactory<RankingViewData, Integer>("completedTasks"));
+        tableViewRanking.setItems(list);
+        tableViewRanking.refresh();
     }
 
     @FXML
