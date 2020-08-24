8d9fdb551ec15ae3e4c67738eb8af2781bc10747
Robert Huber <sopr060.sopra@cs.uni-dortmund.de>
2020-08-24 10:54:55 +0200

    Adds update function for team combobox in NewProjectViewController

--- src/view/MainViewController.java
+++ src/view/MainViewController.java
@@ -278,6 +278,7 @@ public class MainViewController extends BorderPane {
      */
     @FXML
     void onCreateProjectButtonClick(MouseEvent event) {
+    	newProjectViewController.updateTeamCombobox();
         newProjectViewController.showView();
     }
 
